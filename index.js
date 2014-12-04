5d1bae219095a14d33cb834edc3decaf4cf81a84
Marius <marelis951@gmail.com>
2014-12-04 19:17:24 +0200

    Fix sending message to specific client & move sessions list into separate object file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var session = require('./src/session');
-var sessions = [];
+var Session = require('./src/Session');
+var Sessions = require('./src/Sessions');
 
 app.get('/', function(req, res){
   res.sendfile('views/index.html');
@@ -15,71 +15,35 @@ app.get('/public/js/connection.js', function(req, res){
 io.on('connection', function(socket){
 
   socket.on('disconnect', function(){
-    var sesObj = getSessionObject(socket.id);
-    if (sesObj.opponentId != null) {
-      var targetSesObj = getSessionObject(sesObj.opponentId);
-      io.sockets.connected[targetSesObj.socket].emit('unactive');
-      deleteSession(sesObj.opponentId);
-      console.log('Send unactive response to target player');
+    var sesObj = Sessions.getSessionObject(socket.id);
+    if (sesObj != null && sesObj.opponentId != null) {
+      var targetSesObj = Sessions.getSessionObject(sesObj.opponentId);
+      targetSesObj.socket.emit(Session.UNACTIVE);
+      Sessions.deleteSession(sesObj.opponentId);
     }
     console.log('user disconnected');
-    deleteSession(socket.id);
+    Sessions.deleteSession(socket.id);
   });
 
   socket.on('ready', function() {
-    if (!sessionExists(socket.id)) {
-      var targetSesObj = getAvailableSession();
-      createSession(socket);
+    if (!Sessions.sessionExists(socket.id)) {
+      var targetSesObj = Sessions.getAvailableSession();
+      Sessions.createSession(socket);
       console.log(socket.id + ' is ready');
       if (targetSesObj != null) {
-        var sesObj = getSessionObject(socket.id);
+        var sesObj = Sessions.getSessionObject(socket.id);
         sesObj.opponentId = targetSesObj.sessionId;
-        sesObj.state = 'playing';
+        sesObj.state = Session.PLAYING;
         targetSesObj.opponentId = sesObj.sessionId;
-        targetSesObj.state = 'playing';
-        //TODO send response to both players
-        io.sockets.connected[sesObj.socket].emit('playing');
-        io.sockets.connected[targetSesObj.socket].emit('playing');
-        console.log('Available connection between players');
+        targetSesObj.state = Session.PLAYING;
+        sesObj.socket.emit(Session.PLAYING);
+        targetSesObj.socket.emit(Session.PLAYING);
       }
     }
   });
+
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-function getSessionObject(sessionId) {
-  return sessions[sessionId];
-}
-
-function getAvailableSession() {
-  for (var key in sessions) {
-    var sesObj = sessions[key];
-    if (sesObj.state == 'ready') {
-      return sesObj;
-    }
-  }
-  return null;
-}
-
-function sessionExists(sessionId) {
-  return sessions[sessionId] != null;
-}
-
-function createSession(socket) {
-  var sesObj = new session(socket, null, 'ready');
-  sessions[socket.id] = sesObj;
-}
-
-function deleteSession(sessionId) {
-  delete sessions[sessionId];
-}
-
-function printSessions() {
-  console.log('Sessions: ');
-  for (var key in sessions) {
-    console.log(sessions[key].toString());
-  }
-}
--- src/session.js
+++ src/session.js
@@ -1,35 +1,32 @@
 function Session(socket, opponentId, state) {
 	this.socket = socket;
+	this.sessionId = socket.id;
 	this.opponentId = opponentId;
 	this.state = state;
-}
+};
+
+Session.READY = 'ready';
+Session.PLAYING = 'playing';
+Session.UNACTIVE = 'unactive';
 
 Session.prototype.socket = function() {
 	return this.socket;
-}
+};
 
 Session.prototype.sessionId = function() {
-	return this.socket.id;
-}
+	return this.sessionid;
+};
 
 Session.prototype.opponentId = function() {
 	return this.opponentId;
-}
+};
 
 Session.prototype.state = function() {
 	return this.state;
-}
-
-Session.prototype.setState = function(state) {
-	this.state = state;
-}
-
-Session.prototype.setOpponentId = function(opponentId) {
-	this.opponentId = opponentId;
-}
+};
 
 Session.prototype.toString = function() {
 	return 'Session { sessionid='+ this.socket.id +', opponentId='+ this.opponentId +', state='+ this.state +' }';
-}
+};
 
 module.exports = Session;
