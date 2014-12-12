4d20a66e84f4850bcd648315381cb5f356b821b6
Rokas <r.kazlauskas@gmail.com>
2014-12-12 12:19:18 +0200

    Player emit coordinates

--- src/server/player-collection.js
+++ src/server/player-collection.js
@@ -9,9 +9,8 @@ PlayerCollection.list = [];
 PlayerCollection.getPlayerObject = function(PlayerId) {
   return PlayerCollection.list[PlayerId];
 };
-PlayerCollection.insertPlayer = function(PlayerId) {
-  var playerObj = new Player(PlayerId, null, 0, 0);				//
-  PlayerCollection.list[socket.id] = playerObj;
+PlayerCollection.insertPlayer = function(PlayerId, player) {
+  PlayerCollection.list[PlayerId] = player;
 };
 
 PlayerCollection.deletePlayer = function(PlayerId) {
--- src/server/player.js
+++ src/server/player.js
@@ -1,10 +1,19 @@
-function Player(id, number, x, y){
+var socket = io();
+
+function Player(id, opponentId, x, y){
    this.id = id;
+   this.opponentId = opponentId;
    this.x = x;
    this.y = y;
 }
 Player.prototype.getID = function(){
     return this.id;
+}
+Player.prototype.getOpponentId = function(){
+    return this.opponentId;
+}
+Player.prototype.setOpponentId = function(opponentId){
+    this.opponentId = opponentId;
 }
 Player.prototype.getX = function(){
     return this.x;
@@ -18,5 +27,15 @@ Player.prototype.setX = function(x){
 Player.prototype.setY = function(y){
     this.y = y;
 }
+Player.KeyBindings = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
+
+});
+
+
 
 module.exports = Player;
