df40d4d967f86a9dc18369a3f25ee35e2af8db5a
Marius <marelis951@gmail.com>
2014-12-31 14:15:01 +0200

    Add base config object

--- src/client/lib/config.js
+++ src/client/lib/config.js
@@ -1,8 +1,11 @@
+var BaseConfig = require('../../common/base-config');
+
 var Config = module.exports = function() {};
 
-Config.playerMoveSpeed = 5;
-Config.playerAcceleration = 2;
-Config.playerJumpSpeed = 20;
-Config.playerSize = 30;
+Config.playerMoveSpeed = BaseConfig.playerMoveSpeed;
+Config.playerAcceleration = BaseConfig.playerAcceleration;
+Config.playerJumpSpeed = BaseConfig.playerJumpSpeed;
+Config.playerSize = BaseConfig.playerSize;
+
 Config.canvasUpdateInterval = 30;
 Config.canvasMaskColor = '#000000';
---
+++ src/common/base-config.js
@@ -0,0 +1,8 @@
+var BaseConfig = function() {};
+
+BaseConfig.playerMoveSpeed = 5;
+BaseConfig.playerAcceleration = 2;
+BaseConfig.playerJumpSpeed = 20;
+BaseConfig.playerSize = 30;
+
+module.exports = BaseConfig;
