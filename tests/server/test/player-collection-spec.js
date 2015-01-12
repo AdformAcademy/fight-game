49d8af36cc9865cbbcc817b97d5ef67a99b0afd0
Marius <marelis951@gmail.com>
2015-01-12 16:04:58 +0200

    Add player-collection test file

---
+++ tests/server/test/player-collection-spec.js
@@ -0,0 +1,53 @@
+var vows = require('vows');
+var assert = require('assert');
+var PlayerCollection = require('../../../src/server/player-collection.js');
+
+var mockPlayer = {};
+var mockPlayerId = 'mockId';
+
+vows.describe('Player collection').addBatch({
+	'methods': {
+		'list': {
+			'is defined': function () {
+				assert.isTrue(PlayerCollection.list !== undefined);
+			},
+			'is array': function () {
+				assert.isArray(PlayerCollection.list);
+			}
+		},
+		'get player object': {
+			'is defined': function () {
+				assert.isTrue(PlayerCollection.getPlayerObject !== undefined);
+			}
+		},
+		'insert player': {
+			'is defined': function () {
+				assert.isTrue(PlayerCollection.insertPlayer !== undefined);
+			}
+		},
+		'delete player': {
+			'is defined': function () {
+				assert.isTrue(PlayerCollection.deletePlayer !== undefined);
+			}
+		},
+		'print players': {
+			'is defined': function () {
+				assert.isTrue(PlayerCollection.printPlayers !== undefined);
+			}
+		},
+	},
+	'player insert': {
+		'get inserted player': function () {
+			PlayerCollection.insertPlayer(mockPlayerId, mockPlayer);
+			var player = PlayerCollection.getPlayerObject(mockPlayerId);
+			assert.strictEqual(player, mockPlayer);
+		}
+	},
+	'player delete': {
+		'delete inserted player': function () {
+			PlayerCollection.deletePlayer(mockPlayerId);
+			var player = PlayerCollection.getPlayerObject(mockPlayerId);
+			assert.isUndefined(player);
+		}
+	},
+}).export(module);
