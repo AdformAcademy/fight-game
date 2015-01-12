cae59793684e58a86da1eddd8b39b31e4bdb41cf
Marius <marelis951@gmail.com>
2015-01-12 14:42:54 +0200

    Add server config test file

---
+++ tests/server/config-spec.js
@@ -0,0 +1,87 @@
+var vows = require('vows');
+var assert = require('assert');
+
+var Config = require('../../src/server/config.js');
+
+vows.describe('Config').addBatch({
+	'port': {
+		'is defined': function () {
+			assert.isTrue(Config.port !== undefined);
+		}
+	},
+	'player move speed': {
+		'is defined': function () {
+			assert.isTrue(Config.playerMoveSpeed !== undefined);
+		},
+		'is number': function () {
+			assert.isNumber(Config.playerMoveSpeed);
+		},
+		'is positive': function () {
+			assert.isTrue(Config.playerMoveSpeed >= 0);
+		}
+	},
+	'player acceleration': {
+		'is defined': function () {
+			assert.isTrue(Config.playerAcceleration !== undefined);
+		},
+		'is number': function () {
+			assert.isNumber(Config.playerAcceleration);
+		},
+		'is positive': function () {
+			assert.isTrue(Config.playerAcceleration >= 0);
+		}
+	},
+	'player jump speed': {
+		'is defined': function () {
+			assert.isTrue(Config.playerJumpSpeed !== undefined);
+		},
+		'is number': function () {
+			assert.isNumber(Config.playerJumpSpeed);
+		},
+		'is positive': function () {
+			assert.isTrue(Config.playerJumpSpeed >= 0);
+		}
+	},
+	'player size': {
+		'is defined': function () {
+			assert.isTrue(Config.playerSize !== undefined);
+		},
+		'is number': function () {
+			assert.isNumber(Config.playerSize);
+		},
+		'is positive': function () {
+			assert.isTrue(Config.playerSize >= 0);
+		}
+	},
+	'key bindings': {
+		'is defined': function () {
+			assert.isTrue(Config.keyBindings !== undefined);
+		}
+	},
+	'screen width': {
+		'is defined': function () {
+			assert.isTrue(Config.screenWidth !== undefined);
+		},
+		'is number': function () {
+			assert.isNumber(Config.screenWidth);
+		},
+		'is positive': function () {
+			assert.isTrue(Config.screenWidth >= 0);
+		}
+	},
+	'first spawn location': {
+		'is defined': function () {
+			assert.isTrue(Config.firstSpawnLocation !== undefined);
+		}
+	},
+	'second spawn location': {
+		'is defined': function () {
+			assert.isTrue(Config.secondSpawnLocation !== undefined);
+		}
+	},
+	'characters path': {
+		'is defined': function () {
+			assert.isTrue(Config.charactersPath !== undefined);
+		}
+	},
+}).export(module);
